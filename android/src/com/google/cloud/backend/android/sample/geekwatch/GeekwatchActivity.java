03981a625e156e0ca39fedf93f281961f58595ab
David M. Chandler <turbomanage@gmail.com>
2013-04-18 22:56:46 -0400

    removed debugging toast

--- android/src/com/google/cloud/backend/android/sample/geekwatch/GeekwatchActivity.java
+++ android/src/com/google/cloud/backend/android/sample/geekwatch/GeekwatchActivity.java
@@ -121,7 +121,6 @@ public class GeekwatchActivity extends CloudBackendActivity implements
 		setUpMapIfNeeded();
 		// Show in saved location on unlock, resume
 		if (mCurrentLocation != null) {
-			Toast.makeText(this, "does this?", Toast.LENGTH_LONG).show();
 			LatLng latLng = new LatLng(mCurrentLocation.getLatitude(),
 					mCurrentLocation.getLongitude());
 			mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
