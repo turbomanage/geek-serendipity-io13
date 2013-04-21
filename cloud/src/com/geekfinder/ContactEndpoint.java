5287550a463b9a82a8469bcbf1cf73f1c1e1bb00
Brad Abrams <bma@google.com>
2013-04-20 17:44:44 -0700

    updated backend, should work now

---
+++ cloud/src/com/geekfinder/ContactEndpoint.java
@@ -0,0 +1,26 @@
+package com.geekfinder;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiMethod.HttpMethod;
+import java.util.logging.Logger;
+
+import javax.inject.Named;
+
+@Api(name = "contactendpoint")
+public class ContactEndpoint {
+
+    private static final Logger log = Logger.getLogger(ContactEndpoint.class.getName());
+
+	  @ApiMethod(
+		      name = "Contact.SendEmail",
+	      path = "contact/sendemail/{emailAddress}",
+      httpMethod = HttpMethod.POST
+		  )
+	
+		  public void sendEmail(@Named("emailAddress") String emailAddress) {
+		    log.info("An informational message.");
+		    return ;
+		  }
+	
+}
