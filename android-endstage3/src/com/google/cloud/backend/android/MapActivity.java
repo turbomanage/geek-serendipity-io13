89365c02a045a89d386dce37b0dcc7e363388a6b
David M. Chandler <turbomanage@gmail.com>
2013-04-23 13:36:48 -0400

    Checked in android-endstage3 showing username

---
+++ android-endstage3/src/com/google/cloud/backend/android/MapActivity.java
@@ -0,0 +1,58 @@
+package com.google.cloud.backend.android;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends CloudBackendActivity {
+
+	private GoogleMap mMap;
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	private void setUpMap() {
+		mMap.setMyLocationEnabled(true);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		setUpMapIfNeeded();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+		TextView overlay = (TextView) findViewById(R.id.overlay);
+		if (super.getAccountName() != null) {
+			overlay.setText(getAccountName());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_map, menu);
+		return true;
+	}
+
+}
