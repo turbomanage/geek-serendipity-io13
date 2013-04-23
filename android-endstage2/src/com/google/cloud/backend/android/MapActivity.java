aa2e2b152e519fe53fdc416dec35e6024272795a
David M. Chandler <turbomanage@gmail.com>
2013-04-23 12:47:32 -0400

    end stage 2

---
+++ android-endstage2/src/com/google/cloud/backend/android/MapActivity.java
@@ -0,0 +1,46 @@
+package com.google.cloud.backend.android;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends FragmentActivity {
+
+		private GoogleMap mMap;
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	private void setUpMap() {
+		mMap.setMyLocationEnabled(true);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		setUpMapIfNeeded();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_map, menu);
+		return true;
+	}
+
+}
